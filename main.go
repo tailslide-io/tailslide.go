953672d0acfb8db9a061a870c0e3503b6e873f29
Jordan Eggers <jordaneggers@Jordans-MBP.lan>
2022-07-23 15:09:45 -0400

    single jetstream publish working

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
-package tailslide
+package main
 
-import "log"
+import "github.com/nats-io/nats.go"
 
-func LogInfo(message string) {
-	log.Printf("INFO -%v", message)
+func main() {
+	// Connect to NATS
+	nc, _ := nats.Connect("nats://127.0.0.1:4222")
+
+	// Create JetStream Context
+	js, _ := nc.JetStream(nats.PublishAsyncMaxPending(256))
+
+	// Simple Stream Publisher
+	js.Publish("ORDERS.scratch", []byte("hello"))
 }
