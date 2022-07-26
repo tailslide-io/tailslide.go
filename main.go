fa8168c631f1e745ad23ef917a661521204d70f8
Jordan Eggers <jordaneggers@Jordans-MacBook-Pro.local>
2022-07-26 16:48:21 -0400

    basic NATS jetstream connection and some concurrency testing

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
-import "github.com/nats-io/nats.go"
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/nats-io/nats.go"
+)
 
 func main() {
 	// Connect to NATS
 	nc, _ := nats.Connect("nats://127.0.0.1:4222")
+	fmt.Println("should be connected now")
 
 	// Create JetStream Context
 	js, _ := nc.JetStream(nats.PublishAsyncMaxPending(256))
 
+	// js.AddStream(&nats.StreamConfig{
+	// 	Name:     "ORDERS",
+	// 	Subjects: []string{"ORDERS.*"},
+	// })
+
 	// Simple Stream Publisher
 	js.Publish("ORDERS.scratch", []byte("hello"))
+
+	//Simple Async Ephemeral Consumer
+	js.Subscribe("ORDERS.>", func(m *nats.Msg) {
+		fmt.Printf("Received a JetStream message: %s\n", string(m.Data))
+	})
+
+	//this is necessary because otherwise main may finish running before the consumer has a chance to
+	runtime.Goexit()
+
 }
