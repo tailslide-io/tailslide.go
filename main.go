b48c73480830be8813b9681944d031d4e10364e2
Jordan Eggers <jordaneggers@Jordans-MBP.lan>
2022-07-27 12:56:34 -0400

    get json message from correct tailslide jetstream subject

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"runtime"
 
 	"github.com/nats-io/nats.go"
@@ -9,24 +10,23 @@ import (
 
 func main() {
 	// Connect to NATS
-	nc, _ := nats.Connect("nats://127.0.0.1:4222")
+	nc, err := nats.Connect("nats://127.0.0.1:4222", nats.Token("myToken"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("should be connected now")
 
 	// Create JetStream Context
 	js, _ := nc.JetStream(nats.PublishAsyncMaxPending(256))
 
-	// js.AddStream(&nats.StreamConfig{
-	// 	Name:     "ORDERS",
-	// 	Subjects: []string{"ORDERS.*"},
-	// })
-
-	// Simple Stream Publisher
-	js.Publish("ORDERS.scratch", []byte("hello"))
-
 	//Simple Async Ephemeral Consumer
-	js.Subscribe("ORDERS.>", func(m *nats.Msg) {
+	js.Subscribe("apps.1.update.manual", func(m *nats.Msg) {
 		fmt.Printf("Received a JetStream message: %s\n", string(m.Data))
 	})
+
+	// js.Subscribe(">", func(m *nats.Msg) {
+	// 	fmt.Printf("Received a JetStream message: %s\n", m.Subject)
+	// })
 
 	//this is necessary because otherwise main may finish running before the consumer has a chance to
 	runtime.Goexit()
